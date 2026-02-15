9c64f77f9ad032ec346b1afc66e1d7c97c88b82d
John Ray <photographybyjohnray@gmail.com>
2026-02-15 22:01:18 +0000

    Update main.py

--- main-code/main.py
+++ main-code/main.py
@@ -1,5 +1,19 @@
+# Import Python libraries
 import pandas as pd
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+import yfinance as yf
 
-# Yeah, I haven't written the entire code yet, sorry guys :(
+# Import other files (some of these are to be made)
+from regression import Regression
+from neuralnetwork import NeuralNetwork
+from userinterface import UserInterface
+# Makes matplotlib graphs look nice :)
+mpl.rcParams['font.family'] = 'Times New Roman'
+
+"""
+Main Python file
+
+All the other files can be imported into here. This is just here as a basic template for now.
+"""
